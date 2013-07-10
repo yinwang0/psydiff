6a6ff7f0f86a8130b50ab27dae5221e3107405ad
Yin Wang <yinwang0@gmail.com>
2013-07-10 00:20:41 -0700

    removal of copyleft notice from diff.css and nav.js because they will now be inserted into the HTML files

--- nav.js
+++ nav.js
@@ -1,22 +1,3 @@
-// ydiff - a language-aware tool for comparing programs
-// Copyright (C) 2011-2013 Yin Wang (yinwang0@gmail.com)
-
-
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-
-
 // convenience function for document.getElementById().
 window['$']=function(a){return document.getElementById(a)};
 
